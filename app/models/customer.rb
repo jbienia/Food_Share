8a3b058e8d377039e65c9e320495fcbd2dc90457
Jason Bienia <jasonbienia@gmail.com>
2017-03-30 21:30:39 +0000

    Images are displaying. ImageMagick is resizing them in the meals model.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,6 @@ class Customer < ApplicationRecord
   has_many :orders
   validates :full_name, :address, :city, :email, presence: true
   validates :user_name, uniqueness: true
-  #validates :password, length: { minimum: 6 }
+  # validates :password, length: { minimum: 6 }
   validates :email, format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,10 +2,12 @@
 class Meal < ApplicationRecord
   belongs_to :category
   has_attached_file :image,
-                    #url: ':rails_root/app/assets/images/:basename.:extension'
-                    path: ':rails_root/app/assets/images/:basename.:extension'
+                    # url: ':rails_root/app/assets/images/:basename.:extension'
+                    path: ':rails_root/app/assets/images/:basename.:extension',
+                    styles: { small: "259 x 194<" }
   validates :description, :price, presence: true
   validates :price, numericality: { only_integer: true }
+
   validates_attachment_content_type :image, content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
 
   # Displays the description column name of a meal in the timeoutable
