0b9256d0ab43a0456c4d9494608fe4fba9de599d
Jason Bienia <jasonbienia@gmail.com>
2017-04-12 20:47:08 +0000

    Items and quantity are being writting to a shopping cart session varible

--- app/controllers/shopping_cart_controller.rb
+++ app/controllers/shopping_cart_controller.rb
@@ -1,9 +1,19 @@
 class ShoppingCartController < ApplicationController
   def add_to_cart
     puts "HEY JIM"
-    puts params[:search_value]
-    puts params[:meal]
-    puts current_user.id
+    if !session[:shopping_cart]
+      session[:shopping_cart] = {}
+    end
+
+    #stores a session variable mealID => quantity
+    session[:shopping_cart][params[:meal]] = params[:search_value]
+    #={params[:meal] =>params[:search_value]};
+    puts "The hash"
+    puts session[:shopping_cart][params[:meal]]
+    puts session[:shopping_cart]
+    # puts params[:search_value]
+    # puts params[:meal]
+    # puts current_user.id
     redirect_back(fallback_location: "/")
     #redirect_to :back
   end
