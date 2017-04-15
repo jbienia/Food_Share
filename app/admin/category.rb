29041739a77d8dd50b3a238e3bf2d7fb4cea4b3d
Jason Bienia <jasonbienia@gmail.com>
2017-04-15 20:42:58 +0000

    Fixded the Category active admin file

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -12,23 +12,23 @@ ActiveAdmin.register Category do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-
-  form title: 'A custom title' do |f|
-     inputs 'Details' do
-       input :title
-       input :published_at, label: "Publish Post At"
-       li "Created at #{f.object.created_at}" unless f.object.new_record?
-       input :category
-     end
-     panel 'Markup' do
-       "The following can be used in the content below..."
-     end
-     inputs 'Content', :body
-     para "Press cancel to return to the list without saving."
-     actions
-   end
-
-  sidebar :help do
-    'Need help? Email us at help@example.com'
-  end
+  #
+  # form title: 'A custom title' do |f|
+  #    inputs 'Details' do
+  #      input :title
+  #      input :published_at, label: "Publish Post At"
+  #      li "Created at #{f.object.created_at}" unless f.object.new_record?
+  #      input :category
+  #    end
+  #    panel 'Markup' do
+  #      "The following can be used in the content below..."
+  #    end
+  #    inputs 'Content', :body
+  #    para "Press cancel to return to the list without saving."
+  #    actions
+  #  end
+  #
+  # sidebar :help do
+  #   'Need help? Email us at help@example.com'
+  # end
 end
