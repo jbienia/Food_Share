7e7f6a599b89e37447542ec79342dbafe374602f
Jason Bienia <jasonbienia@gmail.com>
2017-04-18 17:10:55 +0000

    Checkout links created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 #  post ':user_id'
+ get '/checkout', to:'checkout#checkout', as: 'checkout'
+
  get '/remove_from_cart/:meal_id', to: 'shopping_cart#remove'
   get '/cart', to:'shopping_cart#display_cart'
   get '/shopping_cart/:meal/', to: 'shopping_cart#add_to_cart', as:'shopping_cart'
