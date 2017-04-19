636f8b9d3f183b5ffbfad46d37e8a75bdba71e22
Jason Bienia <jasonbienia@gmail.com>
2017-04-19 21:40:09 +0000

    Fixed small order bug. Orders now save to the database

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -13,19 +13,19 @@ class CheckoutController < ApplicationController
 
     after_taxes(@tax_total)
 
-    @money_after_taxes = @value
+    @money_after_taxes = session[:final_total]
   end
 
   def place_order
     puts @money_after_taxes
     Order.create(customer_id: session[:user_id].to_i,
-                 meals: session[:shopping_cart].keys,
-                 total: @value * 100)
+                 meals: session[:shopping_cart],
+                 total: session[:final_total].to_f * 100)
 
     redirect_to root_url, notice: 'Thank you for your order'
   end
 
   def after_taxes(tax_total)
-    @value = ((((session[:cart_total].to_f / 100)) * tax_total).round(2) + session[:cart_total].to_f / 100)
+    session[:final_total] = ((((session[:cart_total].to_f / 100)) * tax_total).round(2) + session[:cart_total].to_f / 100)
   end
 end
